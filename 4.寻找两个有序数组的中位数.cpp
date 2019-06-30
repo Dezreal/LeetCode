1afec1735a5142f850488d5a64d34e95dda61f84
Meowch <1311787255@qq.com>
2019-07-01 00:36:32 +0800

    id=4

--- 4.寻找两个有序数组的中位数.cpp
+++ 4.寻找两个有序数组的中位数.cpp
@@ -6,11 +6,58 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>::iterator it;
-        for(it = nums1.begin(); it != nums1.end(); it++) {
-            cout << *it << " ";
+        
+        int m = nums1.size();
+        int n = nums2.size();
+
+        if (m > n) {
+            vector<int> v = nums1;
+            nums1 = nums2;
+            nums2 = v;
+            int t = m;
+            m = n;
+            n = t;
         }
-        return 0;
+
+        int oe = (m + n) % 2; // odd: 1, even: 0
+
+        int pmin = 0;
+        int pmax = m;
+
+        while (true) {
+            int p = (pmin + pmax) / 2;
+            int q = (m + n
+                     + oe /* can be const: 1 */
+                     ) / 2 - p;
+            
+            if (p < m && q > 0 && nums2[q - 1] > nums1[p]) { // p is too small
+                pmin = p + 1;
+            } else if (p > 0 && q < n && nums1[p - 1] > nums2[q]) { // p is too large
+                pmax = p - 1;
+            } else {
+                double left_max;
+                if (p <= 0) {
+                    left_max = nums2[q - 1];
+                } else if(q <= 0) {
+                    left_max = nums1[p - 1];
+                } else {
+                    left_max = nums1[p - 1] >= nums2[q - 1] ? nums1[p - 1] : nums2[q - 1];
+                }
+                if (oe) {
+                    return left_max;
+                }
+
+                double right_min;
+                if (p == m) {
+                    right_min = nums2[q];
+                } else if(q == n) {
+                    right_min = nums1[p];
+                }else {
+                    right_min = nums1[p] <= nums2[q] ? nums1[p] : nums2[q];
+                }
+                return (left_max + right_min) / 2;
+            }
+        }
     }
 };
 
